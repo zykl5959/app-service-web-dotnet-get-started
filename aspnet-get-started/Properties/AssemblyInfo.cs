7324b15e5a6e78b1aa10d528f42d03d304fca36e
Tiago Costa <tiago.costa@outlook.com>
2022-05-18 22:57:09 +0100

    Update to target dotnet 4.8

--- aspnet-get-started/Properties/AssemblyInfo.cs
+++ aspnet-get-started/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("aspnet_get_started")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bc10474c-aadd-4d63-8ee8-e4f66270fd4e")]
+[assembly: Guid("d0d18c37-5a53-4997-b8b3-013c8fbfe955")]
 
 // Version information for an assembly consists of the following four values:
 //
